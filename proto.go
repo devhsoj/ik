8dd255ffd200785456010ef6fe03cf333f3d7df1
Joshua Benfield <josh@benfield.org>
2025-02-22 02:22:20 -0500

    more efficient event serialization, proto version bump

--- proto.go
+++ proto.go
@@ -7,30 +7,29 @@ import (
 	"io"
 )
 
-const ProtoVersion byte = 1
+const ProtoVersion byte = 2
 
 func craftPacketMetadata(version byte, event string, dataLength int) []byte {
-	if len(event) >= 65_535 {
-		event = event[:65_535]
+	if len(event) > 255 {
+		event = event[:255]
 	}
 
-	buf := make([]byte, 1+2+len(event)+4)
+	buf := make([]byte, 1+1+len(event)+4)
 
 	buf[0] = version
+	buf[1] = uint8(len(event))
 
-	binary.LittleEndian.PutUint16(buf[1:], uint16(len(event)))
+	copy(buf[1+1:], event)
 
-	copy(buf[1+2:], event)
-
-	binary.PutVarint(buf[1+2+len(event):], int64(dataLength))
+	binary.PutVarint(buf[1+1+len(event):], int64(dataLength))
 
 	return buf
 }
 
 func readPacketMetadata(r *bufio.Reader) (version byte, event string, dataLength int, err error) {
-	eventLengthBuf := make([]byte, 2)
 	dataLengthBuf := make([]byte, 4)
 
+	var eventLength byte
 	var eventBuf []byte
 	var dataLength_ int64
 
@@ -44,11 +43,13 @@ func readPacketMetadata(r *bufio.Reader) (version byte, event string, dataLength
 		return 0, "", 0, fmt.Errorf("ik: invalid version in packet metadata: %d", version)
 	}
 
-	if _, err = io.ReadFull(r, eventLengthBuf); err != nil {
+	eventLength, err = r.ReadByte()
+
+	if err != nil {
 		return 0, "", 0, err
 	}
 
-	eventBuf = make([]byte, binary.LittleEndian.Uint16(eventLengthBuf))
+	eventBuf = make([]byte, eventLength)
 
 	if _, err = io.ReadFull(r, eventBuf); err != nil {
 		return 0, "", 0, err
