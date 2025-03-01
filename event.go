b41fd10ea53d026e0b838b7e25f6a3496b60de7e
Joshua Benfield <josh@benfield.org>
2025-03-01 15:33:59 -0500

    added event documentation

--- event.go
+++ event.go
@@ -1,5 +1,6 @@
 package ik
 
+// EventHandler is a function that is passed a ServerClient and event data received from a Client.
 type EventHandler = func(c *ServerClient, data []byte) []byte
 
 type eventHandlerMap map[string]EventHandler
